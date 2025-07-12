473ad8323c50efdb320a37e6ff6b84f8e4daae46
TripleJ709 <ijn9975@naver.com>
2025-07-13 04:53:09 +0900

    Remove xcuserdata from tracking

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanProject
+//
+//  Created by 장주진 on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
