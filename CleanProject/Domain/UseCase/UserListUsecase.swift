275182a3be58407eb217d17f81016892c57f21a3
TripleJ709 <ijn9975@naver.com>
2025-07-13 20:08:24 +0900

    update userlistUsecase

--- CleanProject/Domain/UseCase/UserListUsecase.swift
+++ CleanProject/Domain/UseCase/UserListUsecase.swift
@@ -9,9 +9,11 @@ import Foundation
 
 public protocol UserListUsecaseProtocol {
     func fetchUser(query: String, page: Int) async -> Result<UserListResult, NetworkError>
-    func getFavoritUsers() -> Result<UserListItem, CoreDataError>
+    func getFavoritUsers() -> Result<[UserListItem], CoreDataError>
     func saveFavoritUser(user: UserListItem) -> Result<Bool, CoreDataError>
     func deleteFavoritUser(userID: Int) -> Result<Bool, CoreDataError>
+    func checkFavoriteState(fetchUsers: [UserListItem], favoriteUsers: [UserListItem]) -> [(user: UserListItem, isFavorite: Bool)]
+    func converListToDictionary(favoriteUsers: [UserListItem]) -> [String: [UserListItem]]
 }
 
 public struct UserListUsecase: UserListUsecaseProtocol {
@@ -25,7 +27,7 @@ public struct UserListUsecase: UserListUsecaseProtocol {
         await repository.fetchUser(query: query, page: page)
     }
     
-    public func getFavoritUsers() -> Result<UserListItem, CoreDataError> {
+    public func getFavoritUsers() -> Result<[UserListItem], CoreDataError> {
         repository.getFavoritUsers()
     }
     
@@ -37,6 +39,24 @@ public struct UserListUsecase: UserListUsecaseProtocol {
         repository.deleteFavoritUser(userID: userID)
     }
     
+    public func checkFavoriteState(fetchUsers: [UserListItem], favoriteUsers: [UserListItem]) -> [(user: UserListItem, isFavorite: Bool)] {
+        let favoriteSet = Set(favoriteUsers)
+        return fetchUsers.map { user in
+            if favoriteSet.contains(user) {
+                return (user: user, isFavorite: true)
+            } else {
+                return (user: user, isFavorite: false)
+            }
+        }
+    }
     
+    public func converListToDictionary(favoriteUsers: [UserListItem]) -> [String : [UserListItem]] {
+        favoriteUsers.reduce(into: [String : [UserListItem]]()) { dict, user in
+            if let firstString = user.login.first {
+                let key = String(firstString).uppercased()
+                dict[key, default: []].append(user)
+            }
+        }
+    }
 }
 
